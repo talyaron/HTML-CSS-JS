475c7a3cf9b027a2bb3cf11aba4c81aefa209020
Tal Yaron <tal.yaron@gmail.com>
2018-01-25 10:04:14 +0200

    tables and form and dom events

--- JS/browser events/click.js
+++ JS/browser events/click.js
@@ -10,33 +10,70 @@ function changeMyColor(){
 }
 
 function rotate(){  
-  
+
   var rotateAngle = rotateText('secondButton',45);  
   document.getElementById('secondButton').style.transform = 'rotate('+rotateAngle+'deg)';
-  
+
 }
 
 function rotateText(buttonID, rotateBy){
-  
+
   //extract rotation angle
   var currentTextRotation = document.getElementById(buttonID).style.transform;  
   var rotateEndOfNumber = currentTextRotation.search('deg');  
   var rotateAngleString = currentTextRotation.substr(7,rotateEndOfNumber-7);  
   var rotateAngle = parseInt(rotateAngleString);
-  
+
   //check that angle is valid, and correct if invalid
-  
+
   if(!rotateAngle || rotateAngle >= 360){
-    
+
     if (rotateAngle>=360){
       rotateAngle = rotateAngle-360;
     } else{
       rotateAngle = 0;
     }    
   };
-  
+
   //new rotation angle
   rotateAngle += rotateBy;
-  
+
   return rotateAngle;
 }
+
+// --- input text event ---
+
+//set input and output elements
+var inputElement = document.getElementById('inputName');
+var outputElement = document.getElementById('output');
+
+//input from input-text
+inputElement.addEventListener('keyup', function(e){  
+
+  var textInp = '';
+  // if enter send to....
+  if(e.keyCode === 13){
+    textInp = 'send: ';    
+  } 
+
+  //output to output-element
+  textInp += e.target.value; 
+  outputElement.innerText = textInp; 
+
+})
+
+//prevent reload of the page
+inputElement.addEventListener('keydown', function(e){
+  if(e.keyCode === 13){
+    e.preventDefault();
+  }
+})
+
+//submit button
+
+document.getElementById('inputNameSubmit').addEventListener('click', function(){
+
+  outputElement.innerText = 'submit: '+inputElement.value
+
+})
+
