ff3054f0a86b6f46668c482927cda655ef68c5fc
Tal Yaron <tal.yaron@gmail.com>
2018-02-21 09:31:30 +0200

    es6 classes

--- JS/dragNDrop/swipeDemo/script.js
+++ JS/dragNDrop/swipeDemo/script.js
@@ -1,14 +1,18 @@
 // target elements with the "draggable" class
-interact('.draggable')
-  .draggable({
-  axes:'y',
+interact('.draggable').draggable({
+  axes: 'y',
   // enable inertial throwing
   inertia: true,
   // keep the element within the area of it's parent
   restrict: {
     restriction: "parent",
     endOnly: true,
-    elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
+    elementRect: {
+      top: 0,
+      left: 0,
+      bottom: 1,
+      right: 1
+    }
   },
   // enable autoScroll
   autoScroll: true,
@@ -17,36 +21,34 @@ interact('.draggable')
   onmove: dragMoveListener,
   // call this function on every dragend event
   onend: function (event) {
-    var textEl = event.target.querySelector('p');
+    var textEl = event
+      .target
+      .querySelector('p');
 
-    textEl && (textEl.textContent =
-               'moved a distance of '
-               + (Math.sqrt(Math.pow(event.pageX - event.x0, 2) +
-                            Math.pow(event.pageY - event.y0, 2) | 0))
-               .toFixed(2) + 'px');
+    textEl && (textEl.textContent = 'moved a distance of ' + (Math.sqrt(Math.pow(event.pageX - event.x0, 2) + Math.pow(event.pageY - event.y0, 2) | 0)).toFixed(2) + 'px');
   }
 });
 
-function dragMoveListener (event) {
-
+function dragMoveListener(event) {
 
   var target = event.target,
-      // keep the dragged position in the data-x/data-y attributes      
+    // keep the dragged position in the data-x/data-y attributes
 
-      x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
-      //        y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
-      y = (parseFloat(target.getAttribute('data-y')) || 0) ;
+    x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
+    //        y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+    y = (parseFloat(target.getAttribute('data-y')) || 0);
 
-  if(x>200){
+  if (x > 300) {
     console.log(event.target.id);
-    $('#'+event.target.id+'Row').slideUp(300);
-    $('#'+event.target.id+"Row").velocity({height:'20px'})
+    $('#' + event.target.id).hide(400, function () {
+       $('#' + event.target.id + 'Row *').slideUp(600);
+//      $('#' + event.target.id + "Row *").velocity({height: '0px'})
+    });
+
   }
 
   // translate the element
-  target.style.webkitTransform =
-    target.style.transform =
-    'translate(' + x + 'px, ' + y + 'px)';
+  target.style.webkitTransform = target.style.transform = 'translate(' + x + 'px, ' + y + 'px)';
 
   // update the posiion attributes
   target.setAttribute('data-x', x);
