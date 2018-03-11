c0e25f1cb35d371ce4ab527bc0eaa80d7e3e27d0
Tal Yaron <tal.yaron@gmail.com>
2018-03-11 15:30:17 +0200

    react

---
+++ React/React/react-router/src/App.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+import BasicRoute from './BasicRoute';
+
+class App extends Component {
+  render() {
+    return <BasicRoute/>
+  }
+}
+
+export default App;
